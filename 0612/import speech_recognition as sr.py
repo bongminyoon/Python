2c30e85c40a3f9d50f2273a0f9eb270785ce0d5c
bongminyoon <162547615+bongminyoon@users.noreply.github.com>
2024-10-30 09:44:30 +0900

    Delete 0612 directory

--- 0612/import speech_recognition as sr.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-with sr.Microphone() as source :
-    print("말씀해주세요.")
-    audio = r.listen(source)
-
-text = r.recognize_google(audio, language='ko-KR')
